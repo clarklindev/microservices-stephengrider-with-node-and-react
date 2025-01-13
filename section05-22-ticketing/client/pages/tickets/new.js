3236e04201f632043f259de795d14c88d09d68e9
clarklindev <clarklindev@gmail.com>
2025-01-13 11:34:46 +0800

    486. Scaffolding a Form - adding spacing - margin-bottom to form-group so submit is not directly under it

--- section05-22-ticketing/client/pages/tickets/new.js
+++ section05-22-ticketing/client/pages/tickets/new.js
@@ -3,15 +3,15 @@ const NewTicket = () => {
     <div>
       <h1>create a ticket</h1>
       <form>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <label>Title</label>
-          <input className="form-control"/>
+          <input className="form-control" />
         </div>
-        <div className="form-group">
+        <div className="form-group mb-3">
           <label>price</label>
-          <input className="form-control"/>
+          <input className="form-control" />
         </div>
-        
+
         <button className="btn btn-primary">submit</button>
       </form>
     </div>
